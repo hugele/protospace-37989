da6dd5cd8dfd5a4e1dfd9a6d33fa6bdf84d910e2
hugeiemon <knight_of_cups_123@yahoo.co.jp>
2022-05-19 23:43:22 +0900

    未ログイン時のページの遷移

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -52,8 +52,9 @@ class PrototypesController < ApplicationController
   end 
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to root_path
-    end
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
+    redirect_to action: :index
+     end
   end
 end
